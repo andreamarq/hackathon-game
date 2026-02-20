3c89169fe48c80ef843fe617ab48a157e7aa40c6
JamLynDul <jamielynnd24@gmail.com>
2026-02-20 15:17:01 -0600

    Fly enemy
    Added a fly enemy that can hurt the player, causing the game to reset.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,7 +4,6 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
-
+# Exits the game.
 func _on_exit_pressed() -> void:
 	get_tree().quit()

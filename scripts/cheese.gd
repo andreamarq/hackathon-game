57923c3e48f054370740555298ef5f38ba971fe6
JamLynDul <jamielynnd24@gmail.com>
2026-02-21 08:26:30 -0600

    Game manager, cheese counter
    Added a "GameManager" node, allowing the game to keep track of how much cheese the player has collected.

--- scripts/cheese.gd
+++ scripts/cheese.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var game_manager = %GameManager
 
 # "Collects" the cheese.
 func _on_body_entered(_body: Node2D) -> void:
+	game_manager.add_One()
 	animation_player.play("pickup")
---
+++ scripts/game_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var cheese = 0
+
+func add_One():
+	cheese += 1
+	print(cheese)
